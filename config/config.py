3e7d5151a8933347ebf24ceb35d25b33c7cc3f96
Pikachu <116916755+Iro09@users.noreply.github.com>
2023-01-18 17:41:55 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH", "a089161b52f234bb90a6eb915551e8c0")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "5663640542:AAHBqu3DueQoH9-tuxOPlGaq6HqILxTayzs")
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Izzyanna:Izzyr123@cluster0.l0gyy3d.mongodb.net/Cluster0?retryWrites=true&w=majority")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://pikachu:randi@cluster0.tndvlel.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001772857132"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Iro ダ ᴍᴜsɪᴄ")
 
