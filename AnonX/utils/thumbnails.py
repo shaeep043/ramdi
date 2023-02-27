c83a5a21937273fab7915040f69ab29c99c2a15c
Pikachu <116916755+Iro09@users.noreply.github.com>
2023-02-27 16:52:18 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -123,8 +123,8 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('AnonX/assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "ANON x MUSIC", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((10, 10), "IRO x MUSIC", fill="white", font = font1, align ="left") 
+            image4.text((670, 150), "IRO NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
